ce55561e40adb9880f802ffaa232a9eb202c7506
marcelogsp <marcelogsp@hotmail.com>
2014-08-22 12:25:01 -0300

    Commit inicial

---
+++ GGFotos.UnitTest/UnitTestGGFotos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GGFotos.UnitTest
+{
+    [TestClass]
+    public class UnitTestGGFotos
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
